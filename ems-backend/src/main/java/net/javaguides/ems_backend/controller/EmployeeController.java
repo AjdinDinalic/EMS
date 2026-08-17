6f99ee0f1a909efac5802296ce77c1d29bf6b64e
Ajdin Dinalic <adinalic1@etf.unsa.ba>
2026-08-17 23:50:37 +0200

    Added getAllEmployees service

--- ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
@@ -24,4 +26,11 @@ public class EmployeeController {
        EmployeeDto employeeDto= employeeService.getEmployeeById(employeeId);
        return ResponseEntity.ok(employeeDto);
     }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+       List<EmployeeDto> employees = employeeService.getAllEmployees();
+       return ResponseEntity.ok(employees);
+    }
+
 }
