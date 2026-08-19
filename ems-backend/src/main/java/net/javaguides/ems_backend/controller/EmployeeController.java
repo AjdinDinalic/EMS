230797ded9bd50679f7a81196ae20e933172bce2
Ajdin Dinalic <adinalic1@etf.unsa.ba>
2026-08-19 16:39:24 +0200

    Added deleteEmployee service

--- ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -16,27 +16,35 @@ import java.util.List;
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private EmployeeService employeeService;
+
     @PostMapping
-    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
-    public  ResponseEntity<EmployeeDto> getEmployeeById (@PathVariable("id") Long employeeId){
-       EmployeeDto employeeDto= employeeService.getEmployeeById(employeeId);
-       return ResponseEntity.ok(employeeDto);
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
     }
 
     @GetMapping
-    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
-       List<EmployeeDto> employees = employeeService.getAllEmployees();
-       return ResponseEntity.ok(employees);
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,@RequestBody EmployeeDto updatedEmployee){
-       EmployeeDto employee =  employeeService.updateEmployee(employeeId,updatedEmployee);
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee) {
+        EmployeeDto employee = employeeService.updateEmployee(employeeId, updatedEmployee);
         return ResponseEntity.ok(employee);
+    }
+
+    ;
+@DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Delete successful");
     };
 }
--- ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -51,4 +51,12 @@ public class EmployeeServiceImpl implements EmployeeService {
      Employee updated =  employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(updated);
     }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new ResourceNotFoundException("Employee "+employeeId+" not found"));
+
+        employeeRepository.deleteById(employeeId);
+    }
 }
