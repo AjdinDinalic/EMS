87dfb3330e44e7e9a31859b7de01b898ef994d3f
Ajdin Dinalic <adinalic1@etf.unsa.ba>
2026-08-22 12:55:11 +0200

    Added routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,16 +3,24 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListComponent from './components/ListComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
  
 
   return (
     <>
-
+    <BrowserRouter>
     <HeaderComponent/>
-    <ListComponent/>
+    <Routes>
+
+<Route path='/' element={<ListComponent/>}></Route>
+<Route path='/employees' element={<ListComponent/>}></Route>
+    </Routes>
+    
     <FooterComponent/>
+    </BrowserRouter>
+    
     </>
   )
 }
